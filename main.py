f72c4c7d187c3ae5e0a435cc54dc30b8ded10af4
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-11-06 09:56:20 +0000

    final-1ukol

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(array)
 
@@ -11,3 +12,7 @@ print(lenarray)
 #Druhé pole
 arraya = [6, 8, 9, 4, 15, 58, 2, 3, 1, 7]
 print(sum(arraya + array))
+
+array_random=list(range(1,51)) # Random 
+random.shuffle(array_random)
+print (array_random)
