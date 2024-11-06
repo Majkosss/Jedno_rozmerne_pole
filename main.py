4e3b9c9c5f67060e6cf776b139a7f70e4188b095
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-11-06 09:56:12 +0000

    wswda

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(array)
+
+# výměna čísla v poli
+array[5] = 8
+print(array)
+
+#Délka pole
+lenarray = len(array)
+print(lenarray)
+#Druhé pole
+arraya = [6, 8, 9, 4, 15, 58, 2, 3, 1, 7]
+print(sum(arraya + array))
