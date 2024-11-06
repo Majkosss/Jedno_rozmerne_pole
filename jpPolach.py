1509fab2a04df3ebd86ad05180ffa463cd07bc3c
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-11-06 10:18:32 +0000

    final_Polach

---
+++ jpPolach.py
@@ -0,0 +1,23 @@
+#1 Ukol
+array1 = ["Modra", "Cervena", "Zelena", "Cerna", "Bila"]
+print("Pole:", array1)
+
+#2 Ukol
+array1.append("vitr")
+print("Přidán prvek 'vitr':", array1)
+
+#3 Ukol
+array1.remove("Cervena")
+print("Odstranil se 2 prvek:", array1)
+
+#4 Ukol
+
+array1[4] = "slunce"
+print("Zaměnění 5 prvku na 'slunce':", array1)
+
+#5 Ukol
+
+array1.extend(["Bageta", "Kartacek"])
+print("Přidané 2 prvky 'Bageta' a 'Kartacek':", array1)
+
+#6 Ukol - Po každém poli vypsáno printem.
